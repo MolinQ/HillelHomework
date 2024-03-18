31f105bebd9043067376eba4747750947ed1af5d
Kiril Krasnozhon <kirillaidr@gmail.com>
2024-03-18 12:17:18 +0200

    Complete NumbersAndLines and add semicolon to ListOfTypes

---
+++ NumbersAndLines/index.js
@@ -0,0 +1,5 @@
+let firstLine = prompt('Заповніть перший рядок:');
+let secondLine = prompt('Заповніть другий рядок:');
+let thirdLine = prompt('Заповніть третій рядок:');
+
+console.log(firstLine, secondLine, thirdLine);
