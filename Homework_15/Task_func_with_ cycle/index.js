c9336a8ae5bde78ec61e05265cf47ab165f7cf42
Kiril Krasnozhon <kirillaidr@gmail.com>
2024-04-11 01:51:10 +0300

    task func with cycle completed

--- Homework_15/Task_func_with_ cycle/index.js
+++ Homework_15/Task_func_with_ cycle/index.js
@@ -1,28 +1,17 @@
 function getNum() {
   for (let i = 0; i <= 10; i++) {
-    number = prompt("Введіть число більше 100:");
-  
-      if (number === null) {
-        console.log("число введене не коректно!");
-        break;
-      }
-  
-      if (isNaN(number)) {
-        console.log("число введене не коректно!");
+    let number = prompt("Введіть число більше 100:");
+    switch (true) {
+      case number > 100:
+        console.log("Останнє значення", number);
+        return;
+      case number <= 100:
         continue;
-      }
-  
-      if (number < 100) {
-        console.log("число введене не коректно!");
-        continue;
-      } else break;
-  }
-  if (number !== null && number >= 100 && number !== isNaN) {
-    console.log("Ви ввели: " + number);
-  } else if (number === "") {
-    console.log("Ви ввели: " + number);
+      default:
+        console.log("Останнє значення", number);
+        return;
+    }
   }
 }
 
-getNum()
-
+getNum();
