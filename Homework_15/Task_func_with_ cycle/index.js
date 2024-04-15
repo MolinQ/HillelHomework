97764acc25d76505c5c8244752b4b66323c66b19
Kiril Krasnozhon <kirillaidr@gmail.com>
2024-04-15 14:37:42 +0300

    add log in all case and final log

--- Homework_15/Task_func_with_ cycle/index.js
+++ Homework_15/Task_func_with_ cycle/index.js
@@ -1,17 +1,19 @@
 function getNum() {
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i < 10; i++) {
     let number = prompt("Введіть число більше 100:");
     switch (true) {
       case number > 100:
         console.log("Останнє значення", number);
         return;
       case number <= 100:
+        console.log(`Введіть число ще раз, бо число ${number} < 100`);
         continue;
       default:
         console.log("Останнє значення", number);
         return;
     }
   }
+  console.log("Кількість спроб ввести число закінчилася");
 }
 
 getNum();
