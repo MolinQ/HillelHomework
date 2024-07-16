1ca57b70581670fdac4dfdb3210f01c6e36eaf26
Kiril Krasnozhon <kirillaidr@gmail.com>
2024-07-16 11:29:07 +0300

    fix input

--- Homework_24.1/Task_firs_react_app/swapiForm/src/components/form.jsx
+++ Homework_24.1/Task_firs_react_app/swapiForm/src/components/form.jsx
@@ -2,7 +2,7 @@ function Form() {
     return(
         <div  className='container input-group mb-3'>
                 <label className='label p-2' htmlFor="input">https://swapi.dev/api/</label>
-            <input className='form-control' type="text"/>
+            <input className='form-control' type="text" placeholder='people/1/'/>
             <div className="input-group-append">
                 <button className="btn btn-outline-secondary" type="button">Get info</button>
             </div>
